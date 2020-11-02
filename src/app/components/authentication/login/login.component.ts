9826e1d59f33c6cec64706e503b1ca8ff300ffdf
RogerBariles <rogerbariles@gmail.com>
2020-11-02 14:55:47 -0300

    se hizo cambios en la validaciones, cuando se loguea el usuario primero verifica si este usuario si tiene una cuenta creada y si esta habilitado para opinar

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -5,28 +5,30 @@ import { Router } from "@angular/router";
 import { setString } from "@nativescript/core/application-settings";
 import { AuthenticationService } from "../../../services/authentication.service";
 import { SnackBar } from "@nativescript-community/ui-material-snackbar";
+import { PersonsService } from "../../../services/persons.service";
+import { People } from "src/app/models/people";
 @Component({
     selector: "ns-login",
     templateUrl: "./login.component.html",
     styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-
     validPas: boolean;
     validUser: boolean;
-    username: string = '';
-    password: string = '';
+    username: string = "";
+    password: string = "";
     snackbar = new SnackBar();
     spinner: boolean;
     formLogin: FormGroup;
 
     constructor(
+        private peopleService: PersonsService,
         private authService: AuthenticationService,
         private router: Router
     ) {
         this.spinner = false;
-        this.validUser = true;
-        this.validPas = true;
+        this.validUser = false;
+        this.validPas = false;
     }
 
     ngOnInit() {
@@ -47,36 +49,39 @@ export class LoginComponent implements OnInit {
     }
 
     getIn() {
-        this.spinner = true;
-        this.formLogin.patchValue({
-            username: this.username,
-            password: this.password
-          });
-        if (this.formLogin.valid) {
-            this.authService.loguin(this.formLogin).subscribe(
-                (resp: Token) => {
-                    setString("token", resp.id_token);
-                    setString("userLogin", this.username);
-                    this.spinner = false;
-                    this.router.navigate([
-                        "/home/",
-                        this.formLogin.get("username").value,
-                    ]);
-                },
-                (error) => {
-                    this.validPas = true;
-                    this.validUser = true;
-                    this.formLogin.get("username").setValue("");
-                    this.formLogin.get("password").setValue("");
-                    this.showSimpleSnackbar();
-                }
-            );
+        this.validField();
+        if (!this.validPas && !this.validUser) {
+            this.spinner = true;
+            this.formLogin.patchValue({
+                username: this.username,
+                password: this.password,
+            });
+            const msj = "Usuario no habilitado, notifique a administración";
+            if (this.formLogin.valid) {
+                this.peopleService
+                    .getPeopleByCondigoPrs(this.username)
+                    .subscribe(
+                        (resp: People[]) => {
+                            if (resp.length > 0) {
+                                this.loguinVerify();
+                            } else {
+                                this.showSimpleSnackbar(msj);
+                            }
+                        },
+                        (error) => {
+                            this.showSimpleSnackbar(msj);
+                        },
+                        () => {}
+                    );
+            }
+        } else {
+            
         }
     }
-    showSimpleSnackbar() {
+    showSimpleSnackbar(msj) {
         this.snackbar
             .action({
-                message: `Credenciales Incorrectas`,
+                message: `${msj}`,
                 textColor: "white",
                 actionTextColor: "red",
                 backgroundColor: "black",
@@ -88,17 +93,44 @@ export class LoginComponent implements OnInit {
             });
     }
 
-    onTextChange(event){
+    onTextChange(event) {
         this.password = event.value;
-        if(this.password != '') {
+        if (this.password != "") {
             this.validPas = false;
         }
     }
 
     onText(event) {
         this.username = event.value;
-        if(this.username != '') {
-            this.validUser= false;
+        if (this.username != "") {
+            this.validUser = false;
         }
+    }
+
+    validField() {
+        this.validPas = this.password != "" ? false : true;
+        this.validUser = this.username != "" ? false : true;
+    }
+
+    loguinVerify() {
+        this.authService.loguin(this.formLogin).subscribe(
+            (resp: Token) => {
+                setString("token", resp.id_token);
+                setString("userLogin", this.username);
+                this.spinner = false;
+                this.router.navigate([
+                    "/home/",
+                    this.formLogin.get("username").value,
+                ]);
+            },
+            (error) => {
+                this.validPas = true;
+                this.validUser = true;
+                this.formLogin.get("username").setValue("");
+                this.formLogin.get("password").setValue("");
+                const msj = "Credenciales Incorrectas";
+                this.showSimpleSnackbar(msj);
+            }
+        );
     }
 }
